ba38445acbfaf66d5db03bf55a6297beb19d7ad2
Nikko Pisciotti <nikko@bluelacesmusic.com>
2017-05-22 17:34:15 -0700

    added side notes

--- function-apply-practice.js
+++ function-apply-practice.js
@@ -1,6 +1,9 @@
 //A practice in memoization. This was a problem I received during an interview.
 
-//Remember that Function.prototype.apply accepts
+//Remember that Function.prototype.apply accepts two arguments, the first being
+//a context to use as "this." Notice that null may be passed. The second argument
+//is an array-like object that contains all necessary arguments for the function
+//that #apply is being called upon.
 
 function timesTwo(num) {
   return num * 2;
