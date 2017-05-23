ccb956f5101c1a0a303c4e52fd5b32a8665ec899
Nikko Pisciotti <nikko@bluelacesmusic.com>
2017-05-23 14:27:30 -0700

    scramble function working

--- scramblies.js
+++ scramblies.js
@@ -5,18 +5,22 @@
 //should return true;
 
 function scramble(str1, str2) {
-  var split = str1.split(' ').join('').split('');
+  var split1 = str1.split(' ').join('').split('');
+  var split2 = str2.split(' ').join('').split('');
   var cache = {};
 
-  for(var i = 0; i < split.length; i++) {
-    if(cache[split[i]]) cache[split[i]] += split[i];
-    else cache[split[i]] = split[i];
+  for(var i = 0; i < split1.length; i++) {
+    if(cache[split1[i]]) cache[split1[i]].push(split1[i]);
+    else {
+       cache[split1[i]] = [];
+       cache[split1[i]].push(split1[i]);
+     };
   }
 
-  while(split.length) {
-    if(cache[split[0]]) {
-      var removed = split.shift();
-      cache[split[0]] -= removed;
+  while(split2.length) {
+    if(cache[split2[0]]) {
+      cache[split2[0]].shift();
+      split2.shift();
     } else return false;
   }
   return true;
