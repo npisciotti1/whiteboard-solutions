0ceac06c9856118c2cc55619a5a2a96c72afbe1c
Nikko Pisciotti <nikko@bluelacesmusic.com>
2017-05-22 17:48:51 -0700

    desc of #memoize

--- function-apply-practice.js
+++ function-apply-practice.js
@@ -10,22 +10,41 @@
 //of arguments to provide to the "called" function (vs #apply which accepts an array).
 //This is important to note because you can use spread syntax with #call.
 
+
+//First declare a function you wish to memoize. It's important to note that the reason
+//for creating memoization is that it prevents unnecessary computation.
+
 function timesTwo(num) {
+  //pretend this is expensive to compute
   return num * 2;
 }
+
+//Now I declare my memoize function which uses an object to use as our "cache." Notice
+//that #memoize accepts a function. Inside #memoize is another returned function with
+//"args" declared. Notice also how "args" will represent our "num" argument in #timesTwo.
+//We can do this because of JS's scopes and closures.
 
 function memoize(func) {
   var cache = {};
 
-  return function(arg) {
+  return function(args) {
 
-    if (cache[arg]) {
-      return cache[arg];
+    if (cache[args]) {
+      return cache[args];
     }
 
-    var output = func.apply(null, arg);
-    cache[arg] = output;
+    var output = func.apply(null, args);
+    cache[args] = output;
     return output;
   }
+}
 
+//Here is a similar implementation but using Function.prototype.call()
+
+
+function fibonacci(n) {
+  if (n === 0 || n === 1)
+    return n;
+  else
+    return fibonacci(n - 1) + fibonacci(n - 2);
 }
