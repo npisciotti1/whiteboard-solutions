d61c7949d089ff51e1e4922f02cf5b0021af11c6
Nikko Pisciotti <nikko@bluelacesmusic.com>
2017-05-23 15:00:41 -0700

    faster solution, still needs optimizing

--- scramblies.js
+++ scramblies.js
@@ -4,6 +4,26 @@
 //str2 = 'cats';
 //should return true;
 
+
+function scramble(str1, str2) {
+  var split1 = str1.split('');
+  var split2 = str2.split('');
+
+  while(split2.length) {
+    var letterIdx = split1.indexOf(split2[0]);
+    if(letterIdx !== -1) {
+      split2.shift();
+      split1.splice(letterIdx, 1);
+    } else return false;
+  }
+  return true;
+}
+
+
+
+
+
+//less-effective solution using a cache and arrays as properties - does handle whitespace though.
 function scramble(str1, str2) {
   var split1 = str1.split(' ').join('').split('');
   var split2 = str2.split(' ').join('').split('');
