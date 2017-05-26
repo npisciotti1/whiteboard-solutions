5265c3eb53cfa595549d20561aed0f66b98d0536
Nikko Pisciotti <nikko@bluelacesmusic.com>
2017-05-26 16:44:46 -0700

    error handling

--- tortoise-racing.js
+++ tortoise-racing.js
@@ -1,20 +1,20 @@
 function race(v1, v2, g) {
-  if(v1 <= 0 || v2 <= 0 || g <= 0 || v1 >= v2) return null;
   var secs = 0, mins = 0, hours = 0, result = [];
 
   var speedDiff = v2 - v1;
   var timeInHours = speedDiff / g;
-  var timeInSeconds = timeInHours * 3600;
+  var timeInSeconds = Math.floor(timeInHours * 3600);
+  console.log('time in seconds:', timeInSeconds);
 
   while(timeInSeconds >= 60) {
-    if(mins = 60) {
+    if(mins >= 60) {
       hours++
-      mins = 0;
+      mins -= 60;
     }
     timeInSeconds -= 60;
     mins++;
   }
   secs += timeInSeconds;
-  result[0] = hours, result[1] = mins, result[3] = secs;
+  result[0] = hours, result[1] = mins, result[2] = secs;
   return result;
 }
