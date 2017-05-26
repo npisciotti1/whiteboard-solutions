9064d86542ba8cbd5b309e0b2ec015d5e15566cb
Nikko Pisciotti <nikko@bluelacesmusic.com>
2017-05-26 16:50:38 -0700

    working solution

--- tortoise-racing.js
+++ tortoise-racing.js
@@ -2,18 +2,15 @@ function race(v1, v2, g) {
   var secs = 0, mins = 0, hours = 0, result = [];
 
   var speedDiff = v2 - v1;
-  var timeInHours = speedDiff / g;
+  var timeInHours = g / speedDiff;
   var timeInSeconds = Math.floor(timeInHours * 3600);
-  console.log('time in seconds:', timeInSeconds);
 
   while(timeInSeconds >= 60) {
-    if(mins >= 60) {
-      hours++
-      mins -= 60;
-    }
+    if(mins >= 60) hours++, mins = 0;
     timeInSeconds -= 60;
     mins++;
   }
+  if(mins === 60) hours++, mins = 0;
   secs += timeInSeconds;
   result[0] = hours, result[1] = mins, result[2] = secs;
   return result;
