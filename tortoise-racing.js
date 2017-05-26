abb28ec71c527fe4b4e8340139a0511e467a6194
Nikko Pisciotti <nikko@bluelacesmusic.com>
2017-05-26 16:57:49 -0700

    problem description

--- tortoise-racing.js
+++ tortoise-racing.js
@@ -1,4 +1,9 @@
+//The premise for this questions is as follows: given two velocities (v1 and v2), find the amount
+//of time needed for a person running at "v2" velocity to catch up with a person running at "v1" //velocity who has a lead of "g" feet. Velocity is expressed in ft/hour. The output needs to be
+//formatted as such --> [hrs, mins, secs]. Seconds can be rounded to the nearest full number.
+
 function race(v1, v2, g) {
+  if(v1 <= 0 || v2 <= 0 || g <= 0 || v2 <= v1) return null;
   var secs = 0, mins = 0, hours = 0, result = [];
 
   var speedDiff = v2 - v1;
