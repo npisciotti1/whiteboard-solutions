66307b56b242b6ad7f8c3ab763e521ad49c8ccdb
Nikko Pisciotti <nikko@bluelacesmusic.com>
2017-05-26 16:35:01 -0700

    building solution

--- tortoise-racing.js
+++ tortoise-racing.js
@@ -1,4 +1,12 @@
 function race(v1, v2, g) {
   if(v1 <= 0 || v2 <= 0 || g <= 0) return null;
-    // your code
+  if(v1 <= v2) return null;
+  var secs = 0, mins = 0, hours = 0;
+
+  var speedDiff = v2 - v1;
+  var timeInHours = speedDiff / g;
+  var timeInSeconds = timeInHours * 3600;
+  while(timeInSeconds >= 60) {
+    if(mins = 60) hours++; mins = 0;
+
 }
