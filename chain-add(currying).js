8d1a92ee08fdcca840b7e4d59fdb208101b141cf
Nikko Pisciotti <nikko@bluelacesmusic.com>
2017-05-28 22:26:25 -0700

    currying implementation

--- chain-add(currying).js
+++ chain-add(currying).js
@@ -13,3 +13,21 @@
 //  addTwo + 5; // 7
 //  addTwo(3); // 5
 //  addTwo(3)(5); // 10
+
+
+function curry(fx) {
+  var arity = fx.length;
+
+  return function f1() {
+    var args = Array.prototype.slice.call(arguments, 0);
+    if (args.length >= arity) {
+      return fx.apply(null, args);
+    }
+    else {
+      return function f2() {
+        var args2 = Array.prototype.slice.call(arguments, 0);
+        return f1.apply(null, args.concat(args2));
+      }
+    }
+  }
+}
