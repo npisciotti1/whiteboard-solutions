2b84d714a35de3def40e93fb29636649e20cc58b
Nikko Pisciotti <nikko@bluelacesmusic.com>
2017-06-04 14:08:10 -0700

    first solution for strings of equal length

--- CtCI-solutions/ch1/1-2.js
+++ CtCI-solutions/ch1/1-2.js
@@ -2,4 +2,26 @@
 //permutations of each other. I've left a separate solution that will check against
 //strings that are different length
 
-function checkPermutation(str1, str2)
+
+//this ver checks against strings of equal length
+function checkPermutation(str1, str2) {
+  if(str1.length === 0 || str1.length !== str2.length) return false;
+
+  var map = new Map();
+
+  for(var i = 0; i < str1.length; i++) {
+    //this nifty bit of logic I got from the online solutions, basically increments
+    //by 1 if map.get returns a found value, otherwise due to short-circuit evaluation with the '||'
+    //operator, will set to 1.
+    map.set(str1[i], map.get(str1[i] + 1 || 1))
+  }
+
+  for(var i = 0; i < str2.length; i++) {
+    var count = map.get(str2[i]);
+    if(!count) return false;
+    if(count === 1) map.delete(str2[i]);
+    else map.set(str2[i], count - 1);
+  }
+
+  return map.size === 0;
+}
