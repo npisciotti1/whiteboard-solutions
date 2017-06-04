f30bcff7fa31f5fd0cce84caf6d87f4dc3455fd0
Nikko Pisciotti <nikko@bluelacesmusic.com>
2017-06-04 14:01:13 -0700

    added dir for CtCI solutions

---
+++ CtCI-solutions/ch1/1-2.js
@@ -0,0 +1,5 @@
+//This function accepts two strings of equal length and checks to see if they are
+//permutations of each other. I've left a separate solution that will check against
+//strings that are different length
+
+function checkPermutation(str1, str2)
