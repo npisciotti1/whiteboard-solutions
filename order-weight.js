42dbd71fab91d54bc395bc780e1c287794336187
Nikko Pisciotti <nikko@bluelacesmusic.com>
2017-06-19 20:09:21 -0700

    modified solutions

---
+++ order-weight.js
@@ -0,0 +1,18 @@
+
+//incomplete
+function orderWeight(str) {
+  var splitStr = str.split('');
+  var objArr = [];
+
+  function countWeight(num) {
+    let split = num.toString().split('');
+    return split.reduce( (acc, val) => acc += val);
+  }
+
+  for(var i = 0; i < splitStr.length; i++) {
+    objArr.push({
+      val: splitStr[i],
+      weight: countWeight(splitStr[i])
+    });
+  }
+}
--- scramblies.js
+++ scramblies.js
@@ -1,6 +1,6 @@
 //This function accepts two strings and will test to see if the combination of letters in string1 can be used to create string2. No whitespace or punctuation will be used in testing
 
-//str1 = 'tacks';
+//str1 = 'tacos';
 //str2 = 'cats';
 //should return true;
 
