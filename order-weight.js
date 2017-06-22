3dca00eba11ed8ee3e41bc700a6d7972461a6eda
Nikko Pisciotti <nikko@bluelacesmusic.com>
2017-06-21 22:00:13 -0700

    much cleaner solution, almost working

--- order-weight.js
+++ order-weight.js
@@ -1,30 +1,20 @@
 
 function orderWeight(str) {
-  var splitStr = str.split(' ');
-  var objArr = [];
-
-  function countWeight(num) {
-    let split = num.toString().split('');
-    return split.reduce( (acc, val) => acc + val);
-  }
-
-  for(var i = 0; i < splitStr.length; i++) {
-    objArr.push({
-      val: splitStr[i],
-      weight: countWeight(splitStr[i])
-    });
-  }
-
-  objArr.sort((a, b) => {
-    if(a.weight < b.weight) return -1;
-    if(a.weight > b.weight) return 1;
-    return 0;
-  });
-  console.log('objArr', objArr);
-
-  objArr = objArr.map( (el) => {
-    return el.val.toString();
-  });
-
-  return objArr.join(' ');
+  return str
+    .split(' ')
+    .map( value => {
+      return {
+        val: value,
+        weight: value.split('').reduce((acc, curr) => {
+          return parseInt(acc) + parseInt(curr);
+        }, 0)
+      }
+    })
+    .sort((a, b) => {
+      if(a.weight < b.weight) return -1;
+      if(a.weight > b.weight) return 1;
+      return 0;
+    })
+    .map( obj => obj.val)
+    .join(' ');
 }
