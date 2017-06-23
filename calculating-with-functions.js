8cb25da8bf44b0007de0d316eb9e4ed9f680da99
Nikko Pisciotti <nikko@bluelacesmusic.com>
2017-06-22 17:16:42 -0700

    solution working

--- calculating-with-functions.js
+++ calculating-with-functions.js
@@ -1,15 +1,40 @@
-function zero() {}
-function one() {}
-function two() {}
-function three() {}
-function four() {}
-function five() {}
-function six() {}
-function seven() {}
-function eight() {}
-function nine() {}
+function eval(operand, num) {
+  return (operand)
+  ? operand.call(operand, num)
+  : num
+}
 
-function plus() {}
-function minus() {}
-function times() {}
-function dividedBy() {}
+function zero(operand) { return eval(operand, 0)}
+function one(operand) { return eval(operand, 1)}
+function two(operand) { return eval(operand, 2)}
+function three(operand) { return eval(operand, 3)}
+function four(operand) { return eval(operand, 4)}
+function five(operand) { return eval(operand, 5)}
+function six(operand) { return eval(operand, 6)}
+function seven(operand) { return eval(operand, 7)}
+function eight(operand) { return eval(operand, 8)}
+function nine(operand) { return eval(operand, 9)}
+
+function plus(a) {
+  return function(b) {
+    return a + b;
+  }
+}
+
+function minus(a) {
+  return function(b) {
+    return a - b;
+  }
+}
+
+function times(a) {
+  return function(b) {
+    return a * b;
+  }
+}
+
+function dividedBy(a) {
+  return function(b) {
+    return a / b;
+  }
+}
