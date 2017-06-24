8a94ece5c71d4e2fad92182b950ea37c995ba9bc
Nikko Pisciotti <nikko@bluelacesmusic.com>
2017-06-23 22:08:25 -0700

    is-valid-ip.js

--- is-valid-ip.js
+++ is-valid-ip.js
@@ -1,3 +1,11 @@
+/*
+Write an algorithm that will identify valid IPv4 addresses in dot-decimal format. Input to the function is guaranteed to be a single string.
+
+Valid inputs: 1.2.3.4 123.45.67.89
+
+Invalid inputs: 1.2.3 1.2.3.4.5 123.456.78.90 123.045.067.089
+*/
+
 function isValidIP(str) {
   return false;
 }
