0b8585f904d3beb36aaaae4bd8ea096fe9b54962
Nikko Pisciotti <nikko@bluelacesmusic.com>
2017-06-24 20:02:28 -0700

    order-weight.js

--- order-weight.js
+++ order-weight.js
@@ -1,4 +1,7 @@
-/*Create a function that accepts a string of integers and returns the same string reordered such that they are sorted by 'weight.' For example, the number 20 would have a weight of 2. The number 13 would have a weight of 4. Numbers with equal weight must be returned based on which would come first alpha-numerically - '45' comes before '54', '101' comes before '200'.
+/*Create a function that accepts a string of integers and returns the same string reordered such
+that they are sorted by 'weight.' For example, the number 20 would have a weight of 2. The number
+13 would have a weight of 4. Numbers with equal weight must be returned based on which would come
+first alpha-numerically - '45' comes before '54', '101' comes before '200'.
 
 input: '100 10 31 13 9 200 2'
 output: '10 100 2 200 13 31 9'
