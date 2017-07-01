96ac60521fb15f2830cbbf45ad54778ff3daeb17
Nikko Pisciotti <nikko@bluelacesmusic.com>
2017-07-01 14:31:04 -0700

    solution working

--- decoder.js
+++ decoder.js
@@ -8,3 +8,21 @@
 
 //Input: 'Akk ir Vekk!'
 //Output: 'All is Well!'
+
+function decoder(input) {
+	let vowelCodes = [65, 69, 73, 79, 85, 97, 101, 105, 105, 111, 117];
+	let midCharCodes = [91, 92, 93, 94, 95, 96];
+	return input
+		.split('')
+		.map((curVal, idx, arr) => {
+			let charVal = curVal.charCodeAt();
+			if (midCharCodes.indexOf(charVal) > 0) return curval;
+			if (vowelCodes.indexOf(charVal) === -1 && charVal > 65 && charVal < 123) {
+				if (curVal === 'z') return 'a';
+				if (curVal === 'Z') return 'A';
+				return String.fromCharCode(charVal + 1);
+			}
+			return curVal;
+		})
+		.join('');
+}
