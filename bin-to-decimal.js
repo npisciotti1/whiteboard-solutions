04d07d0f65506878a04709228576afa97435d147
Nikko Pisciotti <nikko@bluelacesmusic.com>
2017-07-01 14:25:18 -0700

    refactored solution

--- bin-to-decimal.js
+++ bin-to-decimal.js
@@ -1,7 +1,11 @@
 //This function accepts a binary string and returns it's decimal equivalent.
 
 function convertBinary(binString) {
-	return num.split('').reverse().reduce(function(x, y, i) {
-		return y === '1' ? x + Math.pow(2, i) : x;
-	}, 0);
+	var splitStr = binString.split('').reverse();
+	var result = 0;
+
+	for (var i = 0; i < splitStr.length; i++) {
+		if (splitStr[i] === '1') result += Math.pow(2, i);
+	}
+	return result;
 }
