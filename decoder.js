1907dd86c6357afbb8a9911d9cc371ecc18638db
Nikko Pisciotti <nikko@bluelacesmusic.com>
2017-07-01 14:28:06 -0700

    problem desc

---
+++ decoder.js
@@ -0,0 +1,7 @@
+/* This function accepts a string and returns it according to the following rules:
+- Each character will be changed to the next character in the alphabet.
+- All vowels are preserved (except y)
+- All special characters are preserved
+- Uppercasing is preserved.
+- The letter 'z || Z' will becoming either 'a || A'
+*/
