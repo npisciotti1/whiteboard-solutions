cc96089a625cf8a2c106bee8dcc34604e634b75c
Nikko Pisciotti <nikko@bluelacesmusic.com>
2017-07-06 12:25:24 -0700

    problem desc

---
+++ matrix-negative-integers.js
@@ -0,0 +1,13 @@
+// This function accepts 3 arguments, the first being a vertically and horizontally
+// sorted matrix. The second argument (n) being the number of rows, the third (m)
+// being the number of items in each row.
+
+/* Given:
+  var matrix = [
+    [-10, -9, -7, -5],
+    [-5,  -4, -3, -2],
+    [-2,  -1,  0,  1]
+  ];
+
+output should be: 10 (number of negative ints)
+*/
