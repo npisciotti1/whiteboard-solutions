b79ba15ace5e10cc6a646843e7d5404549f857dd
Nikko Pisciotti <nikko@bluelacesmusic.com>
2017-07-08 20:13:15 -0700

    second implementation

--- flatten-array.js
+++ flatten-array.js
@@ -4,4 +4,16 @@ let arr = [[1,2], [3,4], [4,5]];
 
 let flattened = arr.reduce( (a,b) => a.concat(b));
 
-// Another way of doing it
+// Handling nested arrays;
+
+arr = [1,[2,[3]],[4]];
+
+function flatten(arr) {
+  if(!Array.isArray(arr)) return arr;
+
+  let array = [];
+  for(var i = 0; i < arr.length; i++) {
+    array = array.concat(flatten(arr[i]));
+  }
+  return array;
+}
