b60bb73f73a53caba50d61520875df2512a51fd8
Nikko Pisciotti <nikko@bluelacesmusic.com>
2017-07-10 15:27:11 -0700

    working solution

--- find-odd-int.js
+++ find-odd-int.js
@@ -3,3 +3,22 @@
 
 // Input: [1, 2, 4, 2, 4, 3, 1]
 // Output: 3
+
+function findOdd(arr) {
+    arr = arr.sort();
+    var counter = 1;
+
+    for(var i = 0; i < arr.length; i++) {
+        if(arr[i + 1] === arr[i]) {
+            counter++
+            continue;
+        }
+        //is counter even?
+        if(counter % 2 === 0) {
+            counter = 1;
+            continue;
+        }
+        return arr[i];
+    }
+    return false;
+}
