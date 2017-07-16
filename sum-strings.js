f889e9f28b4af062f4e6b8e88b0f913853d1f800
Nikko Pisciotti <nikko@bluelacesmusic.com>
2017-07-15 18:14:15 -0700

    problem desc

---
+++ sum-strings.js
@@ -0,0 +1,10 @@
+//Given the string representations of two integers, return the string representation
+//of the sum of those integers.
+
+//For example:
+
+// sumStrings('1','2') // => '3'
+
+function sumStrings(a,b) {
+
+}
