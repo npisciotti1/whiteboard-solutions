f96101250d03fff07f821fc1979e610183f95b28
Nikko Pisciotti <nikko@bluelacesmusic.com>
2017-07-15 19:35:46 -0700

    second solution for handling large numbers

--- sum-strings.js
+++ sum-strings.js
@@ -12,3 +12,20 @@ function sumStrings(a,b) {
   }
   return result.toString();
 }
+
+function sumStrings(a, b) {
+  a += '' , b += '';
+  var res = [], carry = 0, lena = a.length, lenb = b.length;
+  if(lena > lenb) {
+    b = new Array(lena - lenb + 1).join('0') + b;
+  } else {
+    a = new Array(lenb - lena + 1).join('0') + a;
+  }
+  for(var i=Math.max(lena, lenb)-1; i>=0; i--) {
+    var add = +(a.charAt(i)) + +(b.charAt(i)) + carry;
+    res.push(add%10);
+    carry = (add - (add%10)) / 10;
+  }
+  if(carry) res.push(carry);
+  return res.reverse().join('');
+}
