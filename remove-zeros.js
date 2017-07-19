34b7beebc9681b1f1741e4d353ddb36272e26091
Nikko Pisciotti <nikko@bluelacesmusic.com>
2017-07-18 20:22:32 -0700

    problem desc

--- remove-zeros.js
+++ remove-zeros.js
@@ -15,3 +15,17 @@
 
 //You are NOT allowed to use any temporary arrays or objects. You are also not allowed
 //to use any Array.prototype or Object.prototype methods.
+
+function removeZeros(array) {
+  // Sort "array" so that all elements with the value of zero are moved to the
+  // end of the array, while the other elements maintain order.
+  // [0, 1, 2, 0, 3] --> [1, 2, 3, 0, 0]
+  // Zero elements also maintain order in which they occurred.
+  // [0, "0", 1, 2, 3] --> [1, 2, 3, 0, "0"]
+
+  // Do not use any temporary arrays or objects. Additionally, you're not able
+  // to use any Array or Object prototype methods such as .shift(), .push(), etc
+
+  // the correctly sorted array should be returned.
+  return array;
+}
